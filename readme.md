44865bc3d207df0944c83fc3b84e54d07ba44c8a
eugene <eugene202020@gmail.com>
2021-06-20 04:14:39 +0100

    flask2commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+A simple web app created with Flask, SQLAlchemy, CSS & HTML to utilize the 
+concept of web form submission, data retrieval using https POST/GET methods.
+(@CODEPLATEAU)
--- secondweb.py
+++ secondweb.py
@@ -24,25 +24,32 @@ def create_table():
     db.create_all()
 
 
-# creating a model for the book table in the diagram
+# creating a model for the book table
 class Book(db.Model):
-    title = db.Column(db.String(80), unique = True, nullable = False, primary_key = True)
+    title = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(80), unique = True, nullable = False, primary_key = True)
+    address = db.Column(db.String(80), unique = True, nullable = False)
     # message = db.Column(db.String(80), unique = True, nullable = False)
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
-
-
+        return "<Title: {}>".format(self.title), "<Email: {}>".format(self.email), "<Address: {}>".format(self.address)
+    
+    
 #create a route decorator
 @secondweb.route("/", methods=["GET", "POST"])
 def home():
 # validating the content of the form. This condition shall be false if the request.form list is empty
     if request.form:
         title_from_form = request.form.get("title") # assigns the content of the title field to the variable
-        book = Book(title=title_from_form) # instance of the Book class. assigned to the 'book' variable
+        email_from_form = request.form.get("email") # assigns the content of the email field to the variable
+        address_from_form = request.form.get("address") # assigns the content of the address field to the variable
+
+                
+        book = Book(title=title_from_form, email=email_from_form, address=address_from_form) # instance of the Book class. assigned to the 'book' variable
+        
         db.session.add(book) # adds the data to the session
         db.session.commit() # this commits the data to the database
     books = Book.query.all() # this retrieves all the contents of the book table.
-    return render_template("home.html", iwe = books) # rendering the html page alongside the queried books to the browser.
+    return render_template("home.html", books = books) # rendering the html page alongside the queried books to the browser.
 
 
 #create a route decorator#
